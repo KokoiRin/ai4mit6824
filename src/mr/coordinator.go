cedf98da63943313183aec6d722e796643ff783c
KokoiRin <186729928+KokoiRin@users.noreply.github.com>
2026-04-09 10:06:05 +0800

    设计了Coordinator的数据结构
    Change-Id: Ieb3e1257f83bad7138ec66dfab475a054db8f5af

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,15 +1,50 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
+type TaskType int
+
+const (
+	Map TaskType = iota
+	Reduce
+)
+
+type TypeStatus int
+
+const (
+	Idle TypeStatus = iota
+	InProgress
+	Completed
+)
+
+type Task struct {
+	Id       int
+	Type     TaskType   // Map 或 Reduce
+	Status   TypeStatus // Idle / InProgress / Completed
+	FileName string     // 仅 Map 用
+}
+
+type Phase int
+
+const (
+	MapPhase Phase = iota
+	ReducePhase
+	DonePhase
+)
 
 type Coordinator struct {
-	// Your definitions here.
-
+	files       []string
+	nMap        int
+	mapTasks    []Task
+	nReduce     int
+	reduceTasks []Task
+	phase       Phase
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -21,7 +56,6 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
-
 
 // start a thread that listens for RPCs from worker.go
 func (c *Coordinator) server(sockname string) {
@@ -42,7 +76,6 @@ func (c *Coordinator) Done() bool {
 
 	// Your code here.
 
-
 	return ret
 }
 
@@ -53,7 +86,6 @@ func MakeCoordinator(sockname string, files []string, nReduce int) *Coordinator 
 	c := Coordinator{}
 
 	// Your code here.
-
 
 	c.server(sockname)
 	return &c
